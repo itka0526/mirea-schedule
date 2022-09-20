1ed01b61392c048b17a4a52276bcd4808302634e
itka0526 <itka0526@gmail.com>
2022-09-21 01:15:33 +0300

    GA

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
 import ScrollToTop from "../components/ScrollToTop";
 import "../styles/globals.css";
+import GoogleAnalytics from "../components/GoogleAnalytics";
 
 function MyApp({ Component, pageProps }) {
     return (
         <>
             <Component {...pageProps} />
             <ScrollToTop />
+            <GoogleAnalytics
+                measurementId={process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}
+            />
         </>
     );
 }
